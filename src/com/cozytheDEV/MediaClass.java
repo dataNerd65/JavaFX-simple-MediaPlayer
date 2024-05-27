b397e32ade3839e0731d47a688b3136f65fc2dfc
dataNerd65 <pkiarie811@gmail.com>
2024-05-27 17:41:38 +0300

    Modifications on error handling mechanism

--- src/com/cozytheDEV/MediaClass.java
+++ src/com/cozytheDEV/MediaClass.java
@@ -1,13 +1,6 @@
 package com.cozytheDEV;
 
-/**
- * BUG ALERT
- * handle exceptions in case media controls buttons are pressed without yet loaded media
- * */
-
-
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
@@ -15,9 +8,10 @@ import javafx.scene.control.Slider;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.scene.layout.HBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
+import javafx.fxml.Initializable;
 
 import java.io.File;
 import java.net.URL;
@@ -32,10 +26,16 @@ public class MediaClass implements Initializable {
     private MediaView mainMediaView;
 
     @FXML
-    Slider theVolumeSlider;
+    private HBox mediaControlContainer;
 
-    MediaPlayer mediaPlayer;
-    Stage stage;
+    @FXML
+    private Slider theVolumeSlider;
+
+    private MediaPlayer mediaPlayer;
+    private Stage stage;
+
+    @FXML
+    private MenuBar menuBar;
 
     @FXML
     public void openMediaFile() {
@@ -43,71 +43,37 @@ public class MediaClass implements Initializable {
 
         fileChooser.setTitle("Select Media To Play");
         fileChooser.getExtensionFilters().addAll(
-                // To filter only specific Video formats
-                // To add more Video formats in future
                 new FileChooser.ExtensionFilter("Audio Files", "*mp3"),
                 new FileChooser.ExtensionFilter("Audio Files", "*m4a"),
                 new FileChooser.ExtensionFilter("Video Files", "*.mp4")
-                //new FileChooser.ExtensionFilter("All Files","*.*")
         );
-
 
         File file = fileChooser.showOpenDialog(stage);
 
         if (file != null) {
             String MEDIA_URL = file.toString();
-            // The regex below only extracts the Tittle part of the media
-
-            /* On Windows OS
-              String [] mediaTitleParts = MEDIA_URL.split(":");
-              String mediaTitleSecondPart = mediaTitleParts [1];
-              System.out.println(mediaTitleSecondPart);
-              String [] MediaTitlePartsMain = mediaTitleSecondPart.split("\\\\");
-              MediaClass the Title part which usually the last String after split
-            */
-
-            // On Linux
-            // String [] MediaTitlePartsMain = MEDIA_URL.split("/");
-            // MediaClass the Title part which usually the last String after split
-            // String MediaTitle = MediaTitlePartsMain [MediaTitlePartsMain.length - 1];
-            // lblMediaTitle.setText(MediaTitle);
-
-            // Correct Way to get Filename
             lblMediaTitle.setText(file.getName());
 
             Media media = new Media(new File(MEDIA_URL).toURI().toString());
 
             mediaPlayer = new MediaPlayer(media);
 
-            //mediaPlayer = new MediaPlayer(new Media(this.getClass().getResource(MEDIA_URL).toExternalForm()));
-
-
-            // Volume Slider
             theVolumeSlider.setValue(mediaPlayer.getVolume() * 100);
             theVolumeSlider.valueProperty().addListener(observable -> mediaPlayer.setVolume(theVolumeSlider.getValue() / 100));
 
-
             mainMediaView.setMediaPlayer(mediaPlayer);
-
-            //Resize Video
-
-           /* DoubleProperty width = mainMediaView.fitWidthProperty();
-            DoubleProperty height = mainMediaView.fitHeightProperty();
-
-            width.bind(Bindings.selectDouble(mainMediaView.sceneProperty(), "width"));
-            height.bind(Bindings.selectDouble(mainMediaView.sceneProperty(), "height"));
-            */
         }
     }
 
-
     @Override
     public void initialize(URL Location, ResourceBundle resources) {
+        if (mediaPlayer != null) {
+            MediaControl mediaControl = new MediaControl(mediaPlayer);
+            mediaControlContainer.getChildren().add(mediaControl);
+        } else {
+            System.out.println("Select a media to play!");
+        }
     }
-
-
-    @FXML
-    MenuBar menuBar;
 
     @FXML
     private void play(){
@@ -117,24 +83,32 @@ public class MediaClass implements Initializable {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Information Dialog");
             alert.setHeaderText(null);
-            System.out.println("Media payer not initialized. Please open a media file first.");
-            alert.setContentText("Media payer not initialized. Please open a media file first.");
+            System.out.println("Media player not initialized. Please open a media file first.");
+            alert.setContentText("Media player not initialized. Please open a media file first.");
 
             alert.showAndWait();
+        }
     }
-}
 
     @FXML
     private void pause(){
-        mediaPlayer.pause();
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+        }
     }
+
     @FXML
     private void stop(){
-        mediaPlayer.stop();
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
     }
+
     @FXML
     private void restart(){
-        mediaPlayer.seek(mediaPlayer.getStartTime());
-        mediaPlayer.play();
+        if (mediaPlayer != null) {
+            mediaPlayer.seek(mediaPlayer.getStartTime());
+            mediaPlayer.play();
+        }
     }
 }
