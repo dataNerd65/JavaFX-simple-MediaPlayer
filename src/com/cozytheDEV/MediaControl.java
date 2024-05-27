1c8b4855e74ca149e5a1c55fe0f0ad0d836348c1
dataNerd65 <pkiarie811@gmail.com>
2024-05-27 17:40:01 +0300

    Added a file and modified it to control the app well

--- src/com/cozytheDEV/MediaControl.java
+++ src/com/cozytheDEV/MediaControl.java
@@ -1,7 +1,42 @@
 package com.cozytheDEV;
 
 import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Priority;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.Region;
+import javafx.scene.media.MediaPlayer;
 
 public class MediaControl extends HBox {
+    private MediaPlayer mediaPlayer;
+    private Slider volumeSlider;
+    private Label currentTime;
+    private Button playButton;
+    private Button pauseButton;
+    private Button stopButton;
+
+    public MediaControl(MediaPlayer mediaPlayer){
+        volumeSlider = new Slider();
+        volumeSlider.setPrefWidth(70);
+        volumeSlider.setMaxWidth(Region.USE_PREF_SIZE);
+        volumeSlider.setMinWidth(30);
+
+        currentTime = new Label();
+        currentTime.setText("00:00");
+
+        playButton = new Button("Play");
+        pauseButton = new Button("Pause");
+        stopButton = new Button("Stop");
+
+        this.getChildren().add(playButton);
+        this.getChildren().add(pauseButton);
+        this.getChildren().add(stopButton);
+        this.getChildren().add(volumeSlider);
+        this.getChildren().add(currentTime);
+
+        HBox.setHgrow(volumeSlider, Priority.ALWAYS);
+
+    }
     
 }
